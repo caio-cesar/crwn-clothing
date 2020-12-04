87bec911ccc9c28d57064cd9dea5aa8a810e390a
Souza <caio.souza@atos.net>
2020-12-03 22:01:10 -0300

    Updated redirect

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -24,8 +24,8 @@ const Header = ({ currentUser }) => (
                 CONTACT
             </Link>
             {
-                currentUser ? 
-                <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+                currentUser && currentUser.userAuth ? 
+                <div className='option' onClick={() => {auth.signOut(); alert('oi'); console.log(currentUser)}}>SIGN OUT</div>
                 :
                 <Link className='option' to='/signin'>SIGN IN</Link>
             }
